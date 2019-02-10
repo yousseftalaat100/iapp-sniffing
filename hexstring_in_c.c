fa5c1f039ed5b69fb649cf2ebd846d93100dcdab
Youssef Talaat <43515166+yousseftalaat100@users.noreply.github.com>
2019-02-10 23:09:44 +0100

    read in CLI+linkedList+hashTable

---
+++ hexstring_in_c.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**STEP 2 (Convert HexString to Integer)**/
+float hexstringtodigit(char str[]){
+    int i=0;
+    int value;
+    int power=0;
+    float total=0;
+    for(i=0; i<strlen(str); i++){
+
+        switch(str[i]){
+            case '0':
+                value = 0;
+                break;
+            case '1':
+                value = 1;
+                break;
+            case '2':
+                value = 2;
+                break;
+            case '3':
+                value = 3;
+                break;
+            case '4':
+                value = 4;
+                break;
+            case '5':
+                value = 5;
+                break;
+            case '6':
+                value = 6;
+                break;
+            case '7':
+                value = 7;
+                break;
+            case '8':
+                value = 8;
+                break;
+            case '9':
+                value = 9;
+                break;
+            case 'a':
+            case 'A':
+                value = 10;
+                break;
+            case 'b':
+            case 'B':
+                value = 11;
+                break;
+            case 'c':
+            case 'C':
+                value = 12;
+                break;
+            case 'd':
+            case 'D':
+                value = 13;
+                break;
+            case 'e':
+            case 'E':
+                value = 14;
+                break;
+            case 'f':
+            case 'F':
+                value = 15;
+                break;
+            default:
+                printf("Something has gone badly wrong\n");
+                break;
+            }
+        // to multiply the base from LSB up to MSB
+        power = strlen(str)-1-i;
+        total = total+value*pow(16, power);
+        printf("%c\t%d\t\t%f\n", str[i], value, value*pow(16, power));
+    }
+    printf("equivalent number is: %f\n", total);
+    return total;
+
+}
+
+
+
+int main(void){
+    char str[12];
+    int i;
+    int value;
+    char hexDigit;
+    float convertedF=0;
+    int converted=0;
+    printf("Please enter a hex-string: ");
+    scanf("%s", &str);
+
+    printf("String length is %d\n", strlen(str));
+
+    /**STEP 1**/
+    for(i=0; i<strlen(str); i++){
+        if(!((str[i]>='0' && str[i]<='9')||
+            (str[i]>='A' && str[i]<='F')||
+            (str[i]>='a' && str[i]<='f'))){
+            printf("Invalid Hex Digit, system must exit\n");
+            system("pause");
+            return 1;
+        }
+    }
+    printf("Hex is: %s\n",str);
+    /**STEP  2**/
+
+    convertedF = hexstringtodigit(str);
+    converted = (int)convertedF;
+    printf("Converted: %d\n", converted);
+
+    return 0;
+}
---
+++ linked_lists101.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HASHTABLE_SIZE 12
+
+/** Linked Lists **/
+typedef struct node{
+	int data;
+	struct node *next;
+}node;
+
+/** #NOTE:
+          {node := struct node} **/
+/** Hash Table **/
+node* get_hashtable(){
+    node* hashtable = calloc(HASHTABLE_SIZE, sizeof(struct node)); // calloc for arrays
+    return hashtable; // return the actual individual heads
+};
+
+/** Inserting Elements  **/
+void insert_new_station(node** head, int new_data){
+    node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head == NULL){
+        *head = new_node;
+    } else {
+        node* current = *head;
+
+        while(current->next!=NULL){
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+}
+
+// Hash Function to get a unique index(location)
+int get_location(int new_value){
+    if(new_value < 0){
+        new_value *= -1;
+    }
+
+    return new_value % HASHTABLE_SIZE;
+}
+
+void insert_into_table(node** hashtable, int new_value){
+    int location = get_location(new_value);
+
+    if(hashtable[location] == NULL){
+        hashtable[location] = malloc(sizeof(node*));
+
+        node* head = NULL;
+        insert_new_station(&head, new_value);
+
+        hashtable[location] = head;
+    }else{
+        node* head = hashtable[location];
+        insert_new_station(&head, new_value);
+
+        hashtable[location] = head;
+    }
+}
+
+// Print the whole table with the linked lists
+void display_table(node** hashtable){
+    int i = 0;
+    for(i = 0; i < HASHTABLE_SIZE; i++) {
+        node* head = hashtable[i];
+
+        printf("%d: ", i);
+
+        if(head == NULL){
+            printf("NULL");
+        }else{
+            node* current = head;
+
+            while(current != NULL){
+                printf("%d ", current->data);
+                current = current = current->next;
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+// Program to create a simple linked list with 3 nodes
+int main(int argc, char*argv[]){
+
+    node** table = get_hashtable();
+
+    insert_into_table(table, 1);
+    insert_into_table(table, 6);
+    insert_into_table(table, 3);
+    insert_into_table(table, 2);
+    insert_into_table(table, 5);
+    insert_into_table(table, 6);
+
+    display_table(table);
+
+//	printf("Hello World of Windows!\n");
+//	struct node* head = NULL;
+//	struct node* second = NULL;
+//	struct node* third = NULL;
+//
+//	// Allocate 3 nodes in the "heap"
+//	head = (struct node*)malloc(sizeof(struct node));
+//	second = (struct node*)malloc(sizeof(struct node));
+//	third = (struct node*)malloc(sizeof(struct node));
+//
+//    head->data = 1; // assign data in first node
+//    head->next = second; // link first node with second
+//
+//    second->data = 2;
+//    second->next = third;
+//
+//    third->data = 3;
+//    third->next = NULL;
+//
+//    insert_new_station(head, 4);
+//    insert_new_station(head, 5);
+//    insert_new_station(head, 6);
+//
+//    /* Print elements of the linked list */
+//    printf("i node: %x\n", third->next->next->next->data);
+
+    // Print the elements of linked list as in array
+
+	return 0;
+}
